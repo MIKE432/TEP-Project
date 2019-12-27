eed43b8f3c4cab1b08a65abd19de2d20dc66797d
MIKE432 <michal.raszczuk.apusoft@gmail.com>
2019-12-27 22:19:44 +0100

    Initial Commit

---
+++ TEP Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TEP Project
+//
+//  Created by Michał Raszczuk on 27/12/2019.
+//  Copyright © 2019 Michał Raszczuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
